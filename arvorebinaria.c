ae266a40cc7abb8ccbc23cde0848c58ccded4990
Jonathas MT <jonathastrindade18@gmail.com>
2026-05-01 20:44:00 -0400

    arvorebinaria

---
+++ arvorebinaria.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+
+No* criarNo(int valor) {
+    No* novo = (No*) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+No* inserir(No* raiz, int valor) {
+    if (raiz == NULL){
+        return criarNo(valor);
+    }
+    if (valor < raiz->valor)
+        raiz->esq = inserir(raiz->esq, valor);
+    else if (valor > raiz->valor)
+        raiz->dir = inserir(raiz->dir, valor);
+
+    return raiz;
+}
+
+
+No* buscar(No* raiz, int valor) {
+    if (raiz == NULL || raiz->valor == valor)
+        return raiz;
+
+    if (valor < raiz->valor)
+        return buscar(raiz->esq, valor);
+    else
+        return buscar(raiz->dir, valor);
+}
+
+void emOrdem(No* raiz) {
+    if (raiz != NULL) {
+        emOrdem(raiz->esq);
+        printf("%d ", raiz->valor);
+        emOrdem(raiz->dir);
+    }
+}
+
+void preOrdem(No* raiz) {
+    if (raiz != NULL) {
+        printf("%d ", raiz->valor);
+        preOrdem(raiz->esq);
+        preOrdem(raiz->dir);
+    }
+}
+
+void posOrdem(No* raiz) {
+    if (raiz != NULL) {
+        posOrdem(raiz->esq);
+        posOrdem(raiz->dir);
+        printf("%d ", raiz->valor);
+    }
+}
+
+int altura(No* raiz) {
+
+    if (raiz == NULL)
+        return 0;
+
+    int esquerda = altura(raiz->esq);
+    int direita = altura(raiz->dir);
+
+    if (esquerda > direita)
+        return esquerda + 1;
+    else
+        return direita + 1;
+}
